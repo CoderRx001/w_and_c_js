149826f18f207ca6223ba9b4bd21733233b1611f
CoderRx001 <carrillo964@gmail.com>
2019-01-01 23:57:00 -0600

    initial commit

---
+++ ver1vid2.js
@@ -0,0 +1,7 @@
+['item 1', 'item 2', 'item 3'] //array
+
+var todos = ['item 1', 'item 2', 'item 3']//this is the variable called todos
+
+//call the variable with ...
+todos
+["item 1", "item 2", "item 3"] //prints the variable
